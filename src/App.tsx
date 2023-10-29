2671f91fa2e9c03abf72d474a808de788044ae4d
violettomania <danielpolya@protonmail.com>
2023-10-29 16:16:29 +0100

    re-init project

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+import Person from './Person';
+import { data } from './data';
+
+function App() {
+  const [people, setPeople] = useState(data);
+
+  const handleClick = () => (people.length ? setPeople([]) : setPeople(data));
+
+  return (
+    <main>
+      <div className='container'>
+        <h2>{`${people.length} Birthdays Today`}</h2>
+        {people.map((person) => (
+          <Person key={person.id} {...person} />
+        ))}
+        <button onClick={handleClick}>
+          {people.length ? 'clear all' : 'reload'}
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
